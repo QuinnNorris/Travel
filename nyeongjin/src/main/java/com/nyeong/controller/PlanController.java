0986ded8cc3e9e1c4fd62b177510a2f8194fcc4c
郑甲第 <1215433978@qq.com>
2018-06-11 21:33:05 +0800

    fix

--- nyeongjin/src/main/java/com/nyeong/controller/PlanController.java
+++ nyeongjin/src/main/java/com/nyeong/controller/PlanController.java
@@ -3,8 +3,12 @@ package com.nyeong.controller;
 import com.nyeong.service.PlanService;
 import com.nyeong.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class PlanController {
     @Autowired
     private PlanService planService;
+
+
 }
--- nyeongjin/src/main/java/com/nyeong/service/PlanService.java
+++ nyeongjin/src/main/java/com/nyeong/service/PlanService.java
@@ -9,10 +9,12 @@ import com.nyeong.util.BaseJson;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Service
 public class PlanService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger( PlanService.class );
