19020d295ccd5dee2caf9a9d12f222b88e265d7b
郑甲第 <1215433978@qq.com>
2018-06-11 20:36:40 +0800

    add usercontroller

--- nyeongjin/src/main/java/com/nyeong/controller/UserController.java
+++ nyeongjin/src/main/java/com/nyeong/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.nyeong.controller;
 
 import com.nyeong.service.UserService;
+import com.nyeong.util.BaseJson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -14,15 +17,70 @@ import org.springframework.web.bind.annotation.RestController;
  * @Version: 1.0
  **/
 
+//todo Logged注解要加么
+
 @RestController
 public class UserController {
 
     @Autowired
     private UserService userService;
 
+    //根据 uid 获得用户信息
+    @Logged({"id"})
     @GetMapping("/user")
-    public String getUserMeg() {
-        return userService.getUserMessage();
+    public BaseJson getUserMeg(@RequestParam("id")id) {
+        return userService.getUserMessage( id );
     }
+
+    //注册用户
+    @Logged({"userName", "password", "phone"})
+    @PostMapping("/register")
+    public BaseJson userRegister(@RequestParam("userName") String userName, @RequestParam("password") String password, @RequestParam("phone") String phone) {
+        return userService.signUpUserMsg( account, password );
+    }
+
+
+    //下面 todo
+
+
+    //用户登录
+    @Logged({"account", "password"})
+    @PostMapping("/login")
+    public BaseJson userLogin(@RequestParam("account") String account, @RequestParam("password") String password) {
+        return userService.userLogin( account, password );
+    }
+
+
+    //现在的逻辑不需要加 user
+//    @Logged({ "userID"})
+//    @PostMapping("/logout")
+//    public BaseJson userLogout( @RequestParam("userID") int userID) {
+//        Token token = new Token( tokenString, userID );
+//        userService.userLogout( token );
+//        return BaseJson.success();
+//    }
+
+
+    //todo 省事的话可以不做这个。。
+//    @Logged({ "userID"})
+//    @GetMapping("/info")
+//    public BaseJson getMyUserInfo( @RequestParam("userID") int userID) {
+//        Token token = new Token( tokenString, userID );
+//        MyUserInfo myUserInfo = userService.getMyUserInfo( token );
+//        return BaseJson.success( myUserInfo );
+//    }
+
+
+    @Logged({"userID", "newPassword", "oldPassword"})
+    @PostMapping("/update/password")
+    public BaseJson updateUserPassword(@RequestParam("userID") int userID,
+                                       @RequestParam("oldPassword") String oldPassword,
+                                       @RequestParam("newPassword") String newPassword) {
+
+        return userService.updateUserPassword( userID, oldPassword, newPassword );
+    }
+
+
+
 
 }
